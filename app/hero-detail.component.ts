a8ae67a3f64610a526579df9dcbfb81f8e4f8dda
Jorge <jotagmzn@gmail.com>
2017-02-07 10:28:54 +0100

    Add dashboard and fixed classes

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,5 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+
 import { Hero } from './hero';
+import { HeroService } from './hero.service';
+
+import 'rxjs/add/operator/switchMap';
 
 @Component({
     selector: 'my-hero-detail',
@@ -15,7 +21,19 @@ import { Hero } from './hero';
         `
 })
 
-export class HeroDetailComponent{
+export class HeroDetailComponent implements OnInit{
     @Input()
     hero: Hero;
+
+    constructor(
+      private heroService : HeroService,
+      private route : ActivatedRoute,
+      private location : Location
+    ){}
+
+    ngInit(): void{
+      this.route.params
+        .switchMap((params: Params) => this.heroService.getHero(+params['id'])).
+        subscribe(hero => this.hero = hero);
+    }
 }
